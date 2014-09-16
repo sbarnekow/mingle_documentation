7d48b97a90f0c05a5320df986d4e8d8531a1ec9d
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-16 14:52:54 -0700

    added fixed navigation to the top of the page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,18 @@
 //= require_tree ./lib
 //= require_tree ./app
+
+$(window).load(function(){
+	var $topNavigation = $('.nav-collapse');
+	var $menuOffsetTop = $topNavigation[0].offsetTop;
+
+	$(document).bind('ready scroll', function(){
+		var documentScroll = $(this).scrollTop();
+		
+		if (documentScroll >=  $menuOffsetTop){
+			$topNavigation.addClass('fixed');
+		} else {
+			$topNavigation.removeClass('fixed');
+		}
+
+	});
+});
