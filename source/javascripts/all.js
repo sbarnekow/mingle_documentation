990ed25c18b47eb599977fcb561237d7dd06994f
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-18 15:12:42 -0700

    added helvetica neue to font pack, also fixed header so that content could slide underneath

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,18 +1,21 @@
 //= require_tree ./lib
 //= require_tree ./app
 
-$(window).load(function(){
-	var $topNavigation = $('.nav-collapse');
-	var $menuOffsetTop = $topNavigation[0].offsetTop;
+// $(window).load(function(){
+// 	var $topNavigation = $('.nav-collapse');
+// 	var $menuOffsetTop = $topNavigation[0].offsetTop;
+// 	var $contentBody = $('.content')
 
-	$(document).bind('ready scroll', function(){
-		var documentScroll = $(this).scrollTop();
+// 	$(document).bind('ready scroll', function(){
+// 		var documentScroll = $(this).scrollTop();
 		
-		if (documentScroll >=  $menuOffsetTop){
-			$topNavigation.addClass('fixed');
-		} else {
-			$topNavigation.removeClass('fixed');
-		}
+// 		if (documentScroll >=  $menuOffsetTop){
+// 			$topNavigation.addClass('fixed');
+// 			// $contentBody.addClass('content-margin');
+// 		} else {
+// 			$topNavigation.removeClass('fixed');
+// 			// $contentBody.removeClass('content-margin');
+// 		}
 
-	});
-});
+// 	});
+// });
